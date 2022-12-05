1e5aa374b1ced3b91c5b21f0244f3c384345a431
modengann <99417989+modengann@users.noreply.github.com>
2022-12-05 11:32:15 -0500

    Update mystery_data.py

--- src/mystery_data.py
+++ src/mystery_data.py
@@ -5,14 +5,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 
 def mystery_data():
-    model = LinearRegression(fit_intercept=False)
-    df = pd.read_csv('src/mystery_data.tsv', sep='\t')
-    Y = df["Y"]
-    #you need to think of them as one giant system of linear equations
-    #hence all the X values go together
-    X = df.loc[:, "X1": "X5"]
-    model.fit(X, Y)
-    return model.coef_
+    pass
 
 def main():
     coefficients = mystery_data()
